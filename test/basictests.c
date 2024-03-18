6fbb7380119c3e96b45b592133279e4c461f6509
Vincent Sanders <vince@kyllikki.org>
2024-03-18 00:33:48 +0000

    update test to replace deprecated checks

--- test/basictests.c
+++ test/basictests.c
@@ -115,9 +115,9 @@ with_simple_context_teardown(void)
 START_TEST (test_lwc_intern_string_ok)
 {
         lwc_string *str = NULL;
-        fail_unless(lwc_intern_string("A", 1, &str) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("A", 1, &str) == lwc_error_ok,
                     "Unable to intern a simple string");
-        fail_unless(str != NULL,
+        ck_assert_msg(str != NULL,
                     "Returned OK but str was still NULL");
 }
 END_TEST
@@ -125,13 +125,13 @@ END_TEST
 START_TEST (test_lwc_intern_string_twice_ok)
 {
         lwc_string *str1 = NULL, *str2 = NULL;
-        fail_unless(lwc_intern_string("A", 1, &str1) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("A", 1, &str1) == lwc_error_ok,
                     "Unable to intern a simple string");
-        fail_unless(str1 != NULL,
+        ck_assert_msg(str1 != NULL,
                     "Returned OK but str was still NULL");
-        fail_unless(lwc_intern_string("B", 1, &str2) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("B", 1, &str2) == lwc_error_ok,
                     "Unable to intern a simple string");
-        fail_unless(str2 != NULL,
+        ck_assert_msg(str2 != NULL,
                     "Returned OK but str was still NULL");
 }
 END_TEST
@@ -139,13 +139,13 @@ END_TEST
 START_TEST (test_lwc_intern_string_twice_same_ok)
 {
         lwc_string *str1 = NULL, *str2 = NULL;
-        fail_unless(lwc_intern_string("A", 1, &str1) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("A", 1, &str1) == lwc_error_ok,
                     "Unable to intern a simple string");
-        fail_unless(str1 != NULL,
+        ck_assert_msg(str1 != NULL,
                     "Returned OK but str was still NULL");
-        fail_unless(lwc_intern_string("A", 1, &str2) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("A", 1, &str2) == lwc_error_ok,
                     "Unable to intern a simple string");
-        fail_unless(str2 != NULL,
+        ck_assert_msg(str2 != NULL,
                     "Returned OK but str was still NULL");
 }
 END_TEST
@@ -157,18 +157,18 @@ static lwc_string *intern_one = NULL, *intern_two = NULL, *intern_three = NULL, 
 static void
 with_filled_context_setup(void)
 {
-        fail_unless(lwc_intern_string("one", 3, &intern_one) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("one", 3, &intern_one) == lwc_error_ok,
                     "Unable to intern 'one'");
-        fail_unless(lwc_intern_string("two", 3, &intern_two) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("two", 3, &intern_two) == lwc_error_ok,
                     "Unable to intern 'two'");
-        fail_unless(lwc_intern_string("three", 5, &intern_three) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("three", 5, &intern_three) == lwc_error_ok,
                     "Unable to intern 'three'");
-        fail_unless(lwc_intern_string("YAY", 3, &intern_YAY) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("YAY", 3, &intern_YAY) == lwc_error_ok,
                     "Unable to intern 'YAY'");
         
-        fail_unless(intern_one != intern_two, "'one' == 'two'");
-        fail_unless(intern_one != intern_three, "'one' == 'three'");
-        fail_unless(intern_two != intern_three, "'two' == 'three'");
+        ck_assert_msg(intern_one != intern_two, "'one' == 'two'");
+        ck_assert_msg(intern_one != intern_three, "'one' == 'three'");
+        ck_assert_msg(intern_two != intern_three, "'two' == 'three'");
 }
 
 static void
@@ -185,10 +185,10 @@ START_TEST (test_lwc_interning_works)
 {
         lwc_string *new_one = NULL;
         
-        fail_unless(lwc_intern_string("one", 3, &new_one) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("one", 3, &new_one) == lwc_error_ok,
                     "Unable to re-intern 'one'");
         
-        fail_unless(new_one == intern_one,
+        ck_assert_msg(new_one == intern_one,
                     "Internalising of the string failed");
 }
 END_TEST
@@ -197,12 +197,12 @@ START_TEST (test_lwc_intern_substring)
 {
         lwc_string *new_hre = NULL, *sub_hre = NULL;
         
-        fail_unless(lwc_intern_string("hre", 3, &new_hre) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("hre", 3, &new_hre) == lwc_error_ok,
                     "Unable to intern 'hre'");
-        fail_unless(lwc_intern_substring(intern_three,
+        ck_assert_msg(lwc_intern_substring(intern_three,
                                          1, 3, &sub_hre) == lwc_error_ok,
                     "Unable to re-intern 'hre' by substring");
-        fail_unless(new_hre == sub_hre,
+        ck_assert_msg(new_hre == sub_hre,
                     "'hre' != 'hre' -- wow!");
 }
 END_TEST
@@ -211,7 +211,7 @@ START_TEST (test_lwc_intern_substring_bad_offset)
 {
         lwc_string *str;
         
-        fail_unless(lwc_intern_substring(intern_three, 100, 1, &str) == lwc_error_range,
+        ck_assert_msg(lwc_intern_substring(intern_three, 100, 1, &str) == lwc_error_range,
                     "Able to intern substring starting out of range");
 }
 END_TEST
@@ -220,14 +220,14 @@ START_TEST (test_lwc_intern_substring_bad_size)
 {
         lwc_string *str;
         
-        fail_unless(lwc_intern_substring(intern_three, 1, 100, &str) == lwc_error_range,
+        ck_assert_msg(lwc_intern_substring(intern_three, 1, 100, &str) == lwc_error_range,
                     "Able to intern substring ending out of range");
 }
 END_TEST
 
 START_TEST (test_lwc_string_ref_ok)
 {
-        fail_unless(lwc_string_ref(intern_one) == intern_one,
+        ck_assert_msg(lwc_string_ref(intern_one) == intern_one,
                     "Oddly, reffing a string didn't return it");
 }
 END_TEST
@@ -249,9 +249,9 @@ END_TEST
 START_TEST (test_lwc_string_isequal_ok)
 {
         bool result = true;
-        fail_unless((lwc_string_isequal(intern_one, intern_two, &result)) == lwc_error_ok,
+        ck_assert_msg((lwc_string_isequal(intern_one, intern_two, &result)) == lwc_error_ok,
                     "Failure comparing 'one' and 'two'");
-        fail_unless(result == false,
+        ck_assert_msg(result == false,
                     "'one' == 'two' ?!");
 }
 END_TEST
@@ -261,16 +261,16 @@ START_TEST (test_lwc_string_caseless_isequal_ok1)
         bool result = true;
         lwc_string *new_ONE;
         
-        fail_unless(lwc_intern_string("ONE", 3, &new_ONE) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("ONE", 3, &new_ONE) == lwc_error_ok,
                     "Failure interning 'ONE'");
         
-        fail_unless((lwc_string_isequal(intern_one, new_ONE, &result)) == lwc_error_ok);
-        fail_unless(result == false,
+        ck_assert((lwc_string_isequal(intern_one, new_ONE, &result)) == lwc_error_ok);
+        ck_assert_msg(result == false,
                     "'one' == 'ONE' ?!");
         
-        fail_unless((lwc_string_caseless_isequal(intern_one, new_ONE, &result)) == lwc_error_ok,
+        ck_assert_msg((lwc_string_caseless_isequal(intern_one, new_ONE, &result)) == lwc_error_ok,
                     "Failure comparing 'one' and 'ONE' caselessly");
-        fail_unless(result == true,
+        ck_assert_msg(result == true,
                     "'one' !~= 'ONE' ?!");
 }
 END_TEST
@@ -280,16 +280,16 @@ START_TEST (test_lwc_string_caseless_isequal_ok2)
         bool result = true;
         lwc_string *new_yay;
         
-        fail_unless(lwc_intern_string("yay", 3, &new_yay) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("yay", 3, &new_yay) == lwc_error_ok,
                     "Failure interning 'yay'");
         
-        fail_unless((lwc_string_isequal(intern_YAY, new_yay, &result)) == lwc_error_ok);
-        fail_unless(result == false,
+        ck_assert((lwc_string_isequal(intern_YAY, new_yay, &result)) == lwc_error_ok);
+        ck_assert_msg(result == false,
                     "'yay' == 'YAY' ?!");
         
-        fail_unless((lwc_string_caseless_isequal(intern_YAY, new_yay, &result)) == lwc_error_ok,
+        ck_assert_msg((lwc_string_caseless_isequal(intern_YAY, new_yay, &result)) == lwc_error_ok,
                     "Failure comparing 'yay' and 'YAY' caselessly");
-        fail_unless(result == true,
+        ck_assert_msg(result == true,
                     "'yay' !~= 'YAY' ?!");
 }
 END_TEST
@@ -298,16 +298,16 @@ START_TEST (test_lwc_string_caseless_isequal_bad)
 {
         bool result = true;
         
-        fail_unless(lwc_string_caseless_isequal(intern_YAY, intern_one, &result) == lwc_error_ok,
+        ck_assert_msg(lwc_string_caseless_isequal(intern_YAY, intern_one, &result) == lwc_error_ok,
                     "Failure comparing 'YAY' and 'one' caselessly");
-        fail_unless(result == false,
+        ck_assert_msg(result == false,
                     "'YAY' ~= 'one' ?!");
 }
 END_TEST
 
 START_TEST (test_lwc_extract_data_ok)
 {
-        fail_unless(memcmp("one",
+        ck_assert_msg(memcmp("one",
                            lwc_string_data(intern_one),
                            lwc_string_length(intern_one)) == 0,
                     "Extracting data ptr etc failed");
@@ -324,10 +324,10 @@ START_TEST (test_lwc_string_is_nul_terminated)
 {
         lwc_string *new_ONE;
 
-        fail_unless(lwc_intern_string("ONE", 3, &new_ONE) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("ONE", 3, &new_ONE) == lwc_error_ok,
                     "Failure interning 'ONE'");
 
-        fail_unless(lwc_string_data(new_ONE)[lwc_string_length(new_ONE)] == '\0',
+        ck_assert_msg(lwc_string_data(new_ONE)[lwc_string_length(new_ONE)] == '\0',
                     "Interned string isn't NUL terminated");
 }
 END_TEST
@@ -337,13 +337,13 @@ START_TEST (test_lwc_substring_is_nul_terminated)
         lwc_string *new_ONE;
         lwc_string *new_O;
 
-        fail_unless(lwc_intern_string("ONE", 3, &new_ONE) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("ONE", 3, &new_ONE) == lwc_error_ok,
                     "Failure interning 'ONE'");
 
-        fail_unless(lwc_intern_substring(new_ONE, 0, 1, &new_O) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_substring(new_ONE, 0, 1, &new_O) == lwc_error_ok,
                     "Failure interning substring 'O'");
 
-        fail_unless(lwc_string_data(new_O)[lwc_string_length(new_O)] == '\0',
+        ck_assert_msg(lwc_string_data(new_O)[lwc_string_length(new_O)] == '\0',
                     "Interned substring isn't NUL terminated");
 }
 END_TEST
@@ -354,13 +354,13 @@ START_TEST (test_lwc_string_tolower_ok1)
         lwc_string *new_ONE;
         lwc_string *new_one;
 
-        fail_unless(lwc_intern_string("ONE", 3, &new_ONE) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("ONE", 3, &new_ONE) == lwc_error_ok,
                     "Failure interning 'ONE'");
-        fail_unless(lwc_string_tolower(new_ONE, &new_one) == lwc_error_ok);
-        fail_unless(lwc_string_isequal(intern_one, new_ONE, &result) == lwc_error_ok);
-        fail_unless(result == false, "'one' == 'ONE' ?!");
-        fail_unless(lwc_string_isequal(intern_one, new_one, &result) == lwc_error_ok);
-        fail_unless(result == true, "'one' != 'one' ?!");
+        ck_assert(lwc_string_tolower(new_ONE, &new_one) == lwc_error_ok);
+        ck_assert(lwc_string_isequal(intern_one, new_ONE, &result) == lwc_error_ok);
+        ck_assert_msg(result == false, "'one' == 'ONE' ?!");
+        ck_assert(lwc_string_isequal(intern_one, new_one, &result) == lwc_error_ok);
+        ck_assert_msg(result == true, "'one' != 'one' ?!");
 }
 END_TEST
 
@@ -370,16 +370,16 @@ START_TEST (test_lwc_string_tolower_ok2)
         lwc_string *new_ONE;
         lwc_string *new_one;
 
-        fail_unless(lwc_intern_string("ONE", 3, &new_ONE) == lwc_error_ok,
+        ck_assert_msg(lwc_intern_string("ONE", 3, &new_ONE) == lwc_error_ok,
                     "Failure interning 'ONE'");
         /* Ensure new_ONE has an insensitive pointer set */
-        fail_unless(lwc_string_caseless_isequal(intern_one, new_ONE, &result) == lwc_error_ok);
-        fail_unless(result == true, "'one' != 'ONE' (caseless) ?!");
-        fail_unless(lwc_string_tolower(new_ONE, &new_one) == lwc_error_ok);
-        fail_unless(lwc_string_isequal(intern_one, new_ONE, &result) == lwc_error_ok);
-        fail_unless(result == false, "'one' == 'ONE' ?!");
-        fail_unless(lwc_string_isequal(intern_one, new_one, &result) == lwc_error_ok);
-        fail_unless(result == true, "'one' != 'one' ?!");
+        ck_assert(lwc_string_caseless_isequal(intern_one, new_ONE, &result) == lwc_error_ok);
+        ck_assert_msg(result == true, "'one' != 'ONE' (caseless) ?!");
+        ck_assert(lwc_string_tolower(new_ONE, &new_one) == lwc_error_ok);
+        ck_assert(lwc_string_isequal(intern_one, new_ONE, &result) == lwc_error_ok);
+        ck_assert_msg(result == false, "'one' == 'ONE' ?!");
+        ck_assert(lwc_string_isequal(intern_one, new_one, &result) == lwc_error_ok);
+        ck_assert_msg(result == true, "'one' != 'one' ?!");
 }
 END_TEST
 
@@ -395,7 +395,7 @@ START_TEST (test_lwc_string_iteration)
 {
         int counter = 0;
         lwc_iterate_strings(counting_cb, (void*)&counter);
-        fail_unless(counter == 4, "Incorrect string count");
+        ck_assert_msg(counter == 4, "Incorrect string count");
 }
 END_TEST
 
